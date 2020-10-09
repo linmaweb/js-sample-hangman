1fc016fae4eb56f03cc90a4a9e92abef65467425
Lin <linmaweb@gmail.com>
2020-10-10 02:17:41 +1000

    components updated

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ let playable = true;
 const correctLetters = [];
 const wrongLetters = [];
 
-function displayWord() {
+const displayWord = () => {
   wordEl.innerHTML = `
     ${selectedWord
       .split("")
@@ -39,7 +39,7 @@ function displayWord() {
   }
 }
 
-function updateWrongLettersEl() {
+const updateWrongLettersEl = () => {
   wrongLettersEl.innerHTML = `
     ${wrongLetters.length > 0 ? "<p>Wrong:</p>" : ""}
     ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
@@ -47,7 +47,6 @@ function updateWrongLettersEl() {
 
   figureParts.forEach((part, index) => {
     const errors = wrongLetters.length;
-
     if (index < errors) {
       part.style.display = "block";
     } else {
@@ -59,15 +58,12 @@ function updateWrongLettersEl() {
     finalMessage.innerText = "Unfortunately you lost...";
     finalMessageRevealWord.innerText = `The word was: ${selectedWord}`;
     popup.style.display = "flex";
-
     playable = false;
   }
 }
 
-// Show notification
-function showNotification() {
+const showNotification = () => {
   notification.classList.add("show");
-
   setTimeout(() => {
     notification.classList.remove("show");
   }, 2000);
@@ -81,7 +77,6 @@ window.addEventListener("keydown", (e) => {
       if (selectedWord.includes(letter)) {
         if (!correctLetters.includes(letter)) {
           correctLetters.push(letter);
-
           displayWord();
         } else {
           showNotification();
@@ -89,7 +84,6 @@ window.addEventListener("keydown", (e) => {
       } else {
         if (!wrongLetters.includes(letter)) {
           wrongLetters.push(letter);
-
           updateWrongLettersEl();
         } else {
           showNotification();
@@ -101,16 +95,11 @@ window.addEventListener("keydown", (e) => {
 
 playAgainBtn.addEventListener("click", () => {
   playable = true;
-
   correctLetters.splice(0);
   wrongLetters.splice(0);
-
   selectedWord = words[Math.floor(Math.random() * words.length)];
-
   displayWord();
-
   updateWrongLettersEl();
-
   popup.style.display = "none";
 });
 
